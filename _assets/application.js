ea93e03fe1d465d21b077b271ebe01ba71cf42f5
You <boycecug@gmail.com>
2017-05-25 11:27:24 +0800

    添加三个缺失文件

---
+++ _assets/application.js
@@ -0,0 +1,2 @@
+$(document).on('turbolinks:load',function(){$('.fluidbox-trigger').fluidbox();if(typeof ga==='function'){ga('send','pageview',location.pathname);}
+sr.reveal('.reveal',{distance:'0',duration:500,easing:'ease-in-out',origin:'top',scale:1,reset:false,viewFactor:0});});window.sr=ScrollReveal({reset:true});
